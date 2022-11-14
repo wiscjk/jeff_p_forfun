a8f1e2a55baf924685176e12f0bab10d7b950af1
wiscjk <37421739+wiscjk@users.noreply.github.com>
2022-11-14 10:34:11 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,331 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Jefflikes</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+  <script>
+      
+ function mefunc() {
+  
+  document.getElementById('invi').style.display='none';
+  document.getElementById('invi2').style.display='block';
+  document.getElementById('invi3').style.display='none';
+
+  
+}
+ function mefunc1() {
+ 
+    document.getElementById('invi').style.display='none';
+    
+    document.getElementById('invi3').style.display='block';
+document.getElementById('invi2').style.display='none';
+document.getElementById('invi7').style.display='none';
+  
+}
+
+ function mefunc3() {
+ 
+    document.getElementById('invi').style.display='none';
+     document.getElementById('invi3').style.display='none';
+    document.getElementById('invi7').style.display='block';
+document.getElementById('invi2').style.display='none';
+  
+}
+ </script>
+  <style>
+  body {
+    position: relative; 
+  }
+   #Entertainment {padding-top:55px;height:900px;color: lightsalmon; background-color: slategrey;}
+   
+   h1{
+       font-size: 4em;
+  color: magenta;
+  font-family: Arial, Helvetica, sans-serif;
+  font-style: italic;
+   }
+   h2{
+       font-size: 3em;
+  color: magenta;
+   }
+
+   #oo{
+     color: blueviolet;  
+     font-size: 3em;
+   }
+.sidenav {
+  height: 100%;
+  width: 160px;
+  position: fixed;
+  z-index: 1;
+  top: 120;
+  left: 0;
+  background-color: darkgreen;
+  overflow-x: hidden;
+  padding-top: 200px;
+}
+
+.sidenav a {
+  padding: 4px 9px 6px 12px;
+  font-size: 22px;
+  color: aqua;
+  display: relative;
+}
+
+.sidenav a:hover {
+  color: pink;
+}
+.center {
+  text-align: center;
+
+}
+.center1 {
+  text-align: center;
+ 
+}
+.center3 {
+  text-align: center;
+ 
+}
+.center4 {
+    margin: auto;
+  width: 80%;
+  height: 120%;
+  
+ 
+ 
+}
+.center11, .center121 {
+    margin: auto;
+  width: 80%;
+  height: 120%;
+  
+ 
+ 
+}
+#invi2, #invi7{
+    text-align: right;
+}
+
+.button {
+  background-color: darkkhaki; 
+  border: outset;
+  color: crimson;
+  padding: 2px 5px;
+  text-align: center;
+}
+.button1 {font-size: 24px;}
+
+
+.button1:hover {
+  background-color: firebrick;
+  color: white;
+}
+#a{
+    color: #49fb35;
+    font-size: 150%;
+}
+
+#meme{
+    background-color: coral;
+}
+#memo{
+    background-color: #CC00FF;
+}
+#circle{
+      width: 100px;
+      height: 100px;
+      background: #33FF00;
+      border-radius: 50%
+}
+
+#carrot{
+    border-bottom: 50px solid aqua;
+	border-left: 55px solid transparent;
+	border-right: 55px solid transparent;
+	height: 0;
+	width: 25px;
+	color: fuchsia;
+	font-weight: bold;
+}
+.center10{
+    background-color: yellow;
+}
+  .cir1{
+    background-color: yellow;
+}
+.page-item{
+    font-weight: bold;
+    font-family: Arial, Helvetica, sans-serif;
+  font-style: italic;
+   
+}
+
+  </style>
+</head>
+<body data-spy="scroll" data-target=".navbar" data-offset="100">
+
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">  
+  <ul class="navbar-nav">
+    <li class="nav-item">
+      <a class="nav-link" href="https://jefflikesto.wordpress.com">About</a>
+    </li>
+    <li class="nav-item dropdown">
+      <a class="nav-link dropdown-toggle" href="https://jefflikesto.wordpress.com" id="navbardrop" data-toggle="dropdown">
+        Fun
+      </a>
+      <div class="dropdown-menu">
+        <a class="dropdown-item" href="test.html">Test</a>
+        <a class="dropdown-item" href="havenot.html">What do I have</a>
+         <a class="dropdown-item" href="simple.html">Simple really</a>
+<a class="dropdown-item" href="royal.html">Complex Not So Much</a>
+      </div>
+    </li>
+    <li class="nav-item dropdown">
+      <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+        More Fun
+      </a>
+      <div class="dropdown-menu">
+        <a class="dropdown-item" href="life.html">Discrepancy</a>
+        <a class="dropdown-item" href="button.html">Buttons</a>
+         <a class="dropdown-item" href="jeff.html">Hope and despair</a>
+<a class="dropdown-item" href="jour.html">Time travel</a>
+<a class="dropdown-item" href="search.html">Turbulent waves</a>
+      </div>
+    </li>
+     <li class="nav-item dropdown">
+      <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+        Music
+      </a>
+      <div class="dropdown-menu">
+        <a class="dropdown-item" href="allegro.html">Allegro</a>
+         </div>
+    </li>
+     <li class="nav-item dropdown">
+      <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+        Poetry
+      </a>
+      <div class="dropdown-menu">
+        <a class="dropdown-item" href="poetry.html">Stanza</a>
+         </div>
+    </li>
+    <li class="nav-item dropdown">
+      <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+        Stories
+      </a>
+      <div class="dropdown-menu">
+        <a class="dropdown-item" href="stories.html">Form</a>
+         </div>
+    </li>
+      <li class="nav-item dropdown">
+      <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+        Codes
+      </a>
+      <div class="dropdown-menu">
+        <a class="dropdown-item" href="ai.html">Artificial Intelligence</a>
+         </div>
+    </li>
+         <li class="nav-item dropdown">
+      <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+        Art
+      </a>
+      <div class="dropdown-menu">
+        <a class="dropdown-item" href="paint.html">Painting</a>
+         </div>
+    </li>
+   
+     </ul>
+
+</nav>
+ 
+<div class="sidenav">
+  <a href="#work">Work</a>
+  <a href="#play">Play</a>
+   <a href="#play">Please</a>
+    <a id="oo" href=love.html>love</a>
+     <a href="#play">Drink</a>
+      <a href="#play">Eat</a>
+       <button id="carrot" onclick="mefunc3()" class="button button31">Carrot</button></br></br>
+       <a href="#play">Rest</a>
+        <a href="#play">Fallen</a>
+         <a href="#play">mindless</a>
+          <a href="#play">garden</a>
+           <a href="#play">cave</a>
+            <a href="#play">sea</a>
+            <a href="#play">filled</a>
+            <a href="#play">filled</a>
+            <a href="#play">not filled</a>
+            <a href="#play">vacant</a>
+            <a href="#play">ecstasy</a>
+           <button id="circle" onclick="mefunc1()" class="button button21">Circle of Influence</button></br></br>
+
+   
+            <a href="#play">ecstasy</a>
+ 
+</div>
+
+  <div class="center">
+      <li><a class="navbar-brand" href="#Entertainment">
+        Entertainment
+      </a></li>
+     
+
+ 
+
+ <div id="Entertainment" class="container-fluid">
+     <div class="center1">
+         <div style="text-align:right">
+         <p1 id="a"></p1>
+         </div>
+
+   <button id = "meme" onclick="mefunc()" class="button button1">Entertain me</button></br></br>
+   <div class="center10">
+   <p id="memo"></p>
+    </div>
+  </div>
+   <div class="center3">
+<img id="invi" src="ent.png" alt="ent" style="width:500px;height:600px;">
+<div class="center4">
+ <img id="invi2" src="notent.png" alt="notent" width="200" height="300" style="display:none;">
+</div>
+<div class="center11">
+ <img id="invi3" src="circle.png" alt="circle" width="200" height="300" style="display:none;">
+</div>
+<div class="center121">
+ <img id="invi7" src="carrot.png" alt="carrot" width="200" height="300" style="display:none;">
+</div>
+  </div>
+
+</div>
+
+</div>
+
+
+
+<nav aria-label="Page navigation example">
+  <ul class="pagination justify-content-center">
+      <ul class="pagination pagination-lg">
+    <li class="page-item disabled">
+    
+    </li>
+    <li class="page-item"><a class="page-link" href="#">love</a></li>
+    <li class="page-item"><a class="page-link" href="notfollow.html">do not</a></li>
+   
+     <li class="page-item"><a class="page-link" href="#">earth</a></li>
+      <li class="page-item"><a class="page-link" href="identity.html">Identity</a></li>
+       <li class="page-item"><a class="page-link" href="memory.html">memory</a></li>
+       <li class="page-item"><a class="page-link" href="remember.html">Remember</a></li>
+  
+    <li class="page-item">
+     
+    </li>
+  </ul>
+</nav>
+ 
+</body>
+</html>
